011b214cefbf047ecd4ccb1e55a4a376ee53c7c8
Wills BNTA Macbook <williamstanistreet0299@gmail.com>
2022-11-27 21:38:47 +0000

    First commit: added constructor to Checker class.

---
+++ src/main/java/ParenthesesChecker.java
@@ -0,0 +1,21 @@
+import java.security.PrivateKey;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class ParenthesesChecker {
+
+    private String teststring;
+    private Stack<Character> stack;
+
+    private List<Character> openingParentheses =  Arrays.asList('(', '');
+
+
+
+    public boolean checkParentheses(String testString){
+        stack = new Stack<Character>();
+
+    }
+
+}
