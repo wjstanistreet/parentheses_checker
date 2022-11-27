480c5518a144682faad6bee83446f1ff77336201
Wills BNTA Macbook <williamstanistreet0299@gmail.com>
2022-11-27 22:22:08 +0000

    added loops through string to create stack of parentheses; added first stack logic

--- src/main/java/ParenthesesChecker.java
+++ src/main/java/ParenthesesChecker.java
@@ -6,16 +6,45 @@ import java.util.Stack;
 
 public class ParenthesesChecker {
 
-    private String teststring;
+    private String testString;
     private Stack<Character> stack;
 
-    private List<Character> openingParentheses =  Arrays.asList('(', '');
+    private List<Character> openingParentheses =  Arrays.asList('(', '<', '{','[');
+    private List<Character> closingParentheses =  Arrays.asList(')', '>', '}',']');
 
 
 
     public boolean checkParentheses(String testString){
         stack = new Stack<Character>();
 
+        for (int i = 0; i < testString.length(); i++){
+            if (testString.charAt(i) == openingParentheses.get(0) || testString.charAt(i) == closingParentheses.get(0)){
+                stack.push(testString.charAt(i));
+            }
+        }
+
+        if (stack.size() % 2 != 0){
+            return false;
+        }
+
+        for (int i = 0; i < stack.size(); i++) {
+            if (stack.peek() == closingParentheses.get(0)) {
+                stack.pop();
+            } else {
+                return false;
+            }
+
+
+            if (stack.peek() == openingParentheses.get(0)) {
+                stack.pop();
+            }
+
+        }
+
+
+
+
+        return false;
     }
 
 }
